6c7c79dd642dbaa3c683b6f363331ff478f0fd24
Jérémy Albrecht <me@jalbrecht.fr>
2023-09-27 19:06:32 +0200

    feat: add provider for email and upload

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,34 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'https://augny-badminton.s3.gra.io.cloud.ovh.net/',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'https://augny-badminton.s3.gra.io.cloud.ovh.net/',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  /* End of snippet */
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
---
+++ config/plugins.ts
@@ -0,0 +1,35 @@
+
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: 'contact@augny-badminton.fr',
+        defaultReplyTo: 'contact@augny-badminton.fr',
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: 'aws-s3', // For community providers pass the full package name (e.g. provider: 'strapi-provider-upload-google-cloud-storage')
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        endpoint: env("AWS_ENDPOINT"),
+        s3ForcePathStyle: false,
+        params: {
+          ACL: env('AWS_ACL', 'public-read'), // 'private' if you want to make the uploaded files private
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+    },
+  },
+});
